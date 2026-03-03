eaefddc90b435b2b4bc1e5be4da67038d5423566
Stephen Maldima <maldima00@gmail.com>
2026-03-03 04:34:32 +0200

    updated the hash_password.py with actual passwords

--- web_dashboard/hash_passwords.py
+++ web_dashboard/hash_passwords.py
@@ -1,7 +1,7 @@
 import streamlit_authenticator as stauth
 
 # Replace these with the actual passwords you want to give your officers
-passwords_to_hash = ['officer1_password', 'officer2_password', 'admin_password']
+passwords_to_hash = ['secureAdmin99', 'secureAdmin98', 'secureAdmin97']
 
 hashed_passwords = stauth.Hasher(passwords_to_hash).generate()
 print(hashed_passwords)
